efe5a19b81cc1bfc541b3d1124a766dd3610cb91
MARCO MĀLÏK <am.6180588@gmail.com>
2026-02-25 06:38:22 +0500

    Add files via upload

---
+++ plugins/owner.js
@@ -0,0 +1,65 @@
+const { cmd } = require('../command');
+const config = require('../config');
+
+cmd({
+    pattern: "owner",
+    react: "✅", 
+    desc: "Get owner number",
+    category: "main",
+    filename: __filename
+}, 
+async (conn, mek, m, { from }) => {
+    try {
+        const ownerNumber = config.OWNER_NUMBER; // Fetch owner number from config
+        const ownerName = config.OWNER_NAME;     // Fetch owner name from config
+
+        const vcard = 'BEGIN:VCARD\n' +
+                      'VERSION:3.0\n' +
+                      `FN:${ownerName}\n` +  
+                      `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` +923106052513
+                      'END:VCARD';
+
+        // Send the vCard
+        const sentVCard = await conn.sendMessage(from, {
+            contacts: {
+                displayName: ownerName,
+                contacts: [{ vcard }]
+            }
+        });
+
+        // Send the owner contact message with image and audio
+        await conn.sendMessage(from, {
+            image: { url: 'https://i.ibb.co/vxMX5vTQ/IMG-20260115-WA0000-1771265434749.jpg' }, // Image URL from your request
+            caption: `╭━━〔 *MARCO_MĀLÏK*
+┃◈╭─────────────·๏
+┃◈┃• *Here is the owner details*
+┃◈┃• *Name* - ${ownerName}
+┃◈┃• *Number* ${ownerNumber}
+┃◈┃• *Version*: 2.0.0 Beta
+┃◈└───────────┈⊷
+╰──────────────┈⊷
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ MARCO_MĀLÏK ❣️*`, // Display the owner's details
+            contextInfo: {
+                mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '',
+                    newsletterName: '*MARCO_MĀLÏK*',
+                    serverMessageId: 143
+                }            
+            }
+        }, { quoted: mek });
+
+        // Send audio as per your request
+        await conn.sendMessage(from, {
+            audio: { url: 'https://files.catbox.moe/5kkxwz.mpeg' }, // Audio URL
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+
+    } catch (error) {
+        console.error(error);
+        reply(`An error occurred: ${error.message}`);
+    }
+});
